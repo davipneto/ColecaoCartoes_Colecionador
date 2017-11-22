5fb7e69328a5faedf389f05ea7ebe374e0a84933
Davi Pereira Neto <davineto@alunos.utfpr.edu.br>
2017-11-22 16:19:06 -0200

    arquivos iniciais

---
+++ src/colecaocartoes_colecionador/ColecionadorImpl.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecaocartoes_colecionador;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author davi
+ */
+public class ColecionadorImpl extends UnicastRemoteObject implements InterfaceColecionador {
+    
+    InterfaceCoordenador coord;
+    
+    public ColecionadorImpl() throws RemoteException {
+        try {
+            Registry refSN = LocateRegistry.getRegistry("localhost", 1099);
+            //pega no serviços de nomes a referência do servidor
+            coord = (InterfaceCoordenador) refSN.lookup("RefCoordenador");
+        } catch (NotBoundException ex) {
+            Logger.getLogger(ColecionadorImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (AccessException ex) {
+            Logger.getLogger(ColecionadorImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public boolean desejaEfetivar() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void efetivar() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void abordar() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
